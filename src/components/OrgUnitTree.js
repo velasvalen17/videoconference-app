09e24ed2a8e71b004a9bc0430d802e33d8fa9978
velasvalen17 <velasvalen17@gmail.com>
2020-07-01 08:32:10 +0200

    OrgUnitTree populated and programDropdown refactor

--- src/components/OrgUnitTree.js
+++ src/components/OrgUnitTree.js
@@ -1,24 +1,41 @@
 import React from "react";
 import { OrganisationUnitTree } from "@dhis2/ui";
-
-const Wrapper = () => {
-  return <OrganisationUnitTree name="Root org unit" roots="ImspTQPwCqd" />;
-};
+import { useDataQuery } from "@dhis2/app-runtime";
 
 const ORGUNIT_QUERY = {
-  programs: {
+  orgUnits: {
     resource: "organisationUnits",
+    id: "ImspTQPwCqd",
     params: {
       paging: false,
-      fields: "id, displayName",
+      includeChildren: true,
+      includeDescendants: true,
+      fields: "id, path, children, displayName",
     },
   },
 };
 
 export const OrgUnitTree = () => {
+  const { loading, error, data } = useDataQuery(ORGUNIT_QUERY);
   return (
     <React.Fragment>
-      <h1>Checking</h1>
+      <p>Select an organisation unit from the tree:</p>
+      {// display that the data is being loaded
+      // when loading is true
+      loading && "Loading..."}
+
+      {// display the error message
+      // is an error occurred
+      error && error.message}
+
+      {// if there is any data available
+      data?.orgUnits?.organisationUnits && (
+        <OrganisationUnitTree
+          name="Root org unit"
+          data={data}
+          roots="ImspTQPwCqd"
+        />
+      )}
     </React.Fragment>
   );
 };
---
+++ src/components/ProgramDropdown.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useDataQuery } from "@dhis2/app-runtime";
+import { SingleSelect, SingleSelectOption } from "@dhis2/ui";
+
+const PROGRAMS_QUERY = {
+  programs: {
+    resource: "programs",
+    params: {
+      paging: false,
+      fields: "id, displayName",
+    },
+  },
+};
+
+export const ProgramDropdown = () => {
+  const [program, setProgram] = useState("");
+
+  const { loading, error, data } = useDataQuery(PROGRAMS_QUERY);
+
+  const handleChange = (event) => {
+    setProgram(event.selected);
+    console.log(event.selected);
+  };
+  return (
+    <React.Fragment>
+      <p> Select a tracker program from the list: </p>
+      {// display that the data is being loaded
+      // when loading is true
+      loading && "Loading..."}
+
+      {// display the error message
+      // is an error occurred
+      error && error.message}
+
+      {// if there is any data available
+      data?.programs?.programs && (
+        <SingleSelect onChange={handleChange}>
+          {data.programs.programs.map(({ id, displayName }) => (
+            <SingleSelectOption key={id} label={displayName} value={id} />
+          ))}
+        </SingleSelect>
+      )}
+    </React.Fragment>
+  );
+};
